ec8854860df7d64fb394c6e90f6bfa9fa74735f5
Charles <charlesbai321@gmail.com>
2020-05-14 21:28:34 -0700

    update puzzle js to include more edge cases

--- puzzle.js
+++ puzzle.js
@@ -9,13 +9,23 @@ for(let i = 0; i < arr.length; i++){
     n.push(arr[i].children[0].href)
 }
 let m = [] // [{url: "", size: ""}]
-regex1 = /psix=([0-9]+)/
-regex2 = /psiy=([0-9]+)/
+let regex1 = /psix=([0-9]+)/
+let regex2 = /psiy=([0-9]+)/
+let regex3 = /Size: ([0-9]+)x([0-9]+)/
 for(let i = 0; i < n.length; i++){
     $.ajax({url: n[i]})
         .done(function(data){
-           x = parseInt(data.match(regex1)[1])
-            y = parseInt(data.match(regex2)[1])
+            let x, y
+            let match = data.match(regex1)
+            if(match){
+                x = parseInt(match[1])
+                y = parseInt(data.match(regex2)[1])
+            } else {
+                let match3 = data.match(regex3)
+
+                x = parseInt(match3[1])
+                y = parseInt(match3[2])
+            }
             m.push({url: n[i], size: x*y})
         })
 }
